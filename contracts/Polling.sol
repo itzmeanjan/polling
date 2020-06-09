870c356e39f52a27734c5dc9c6d8bd97787f687e
Anjan Roy <anjanroy@yandex.com>
2020-06-09 12:12:43 +0530

    Added two basic structs for holding information regarding polling
    - Poll struct
    - User struct

--- contracts/Polling.sol
+++ contracts/Polling.sol
@@ -1,7 +1,21 @@
 pragma solidity ^0.6.6;
 
+
 contract Polling {
     address public author;
+
+    struct Poll {
+        bytes32 id;
+        address creator;
+        string content;
+        uint256 timeStamp;
+    }
+
+    struct User {
+        string name;
+        address identifier;
+        Poll[] polls;
+    }
 
     constructor() public {
         author = msg.sender;
