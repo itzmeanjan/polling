7c93cc033db8e3d43293cd903275960361844b5a
Anjan Roy <anjanroy@yandex.com>
2020-06-15 14:27:35 +0530

    Refactored code wit es6 arrow function syntax

--- dapp/src/bridge.js
+++ dapp/src/bridge.js
@@ -11,42 +11,36 @@ class Bridge {
     // it'll call contract method for setting max poll option;
     // 
     // poll count needs to be >=2 && <=16
-    setMaxPollOptionCount(count) {
-        return new Promise((resolve, reject) => {
-            if (!(count >= 2 && count <= 16)) {
-                reject('count must be >=2 && <=16');
-            }
+    setMaxPollOptionCount = (count) => new Promise((resolve, reject) => {
+        if (!(count >= 2 && count <= 16)) {
+            reject('count must be >=2 && <=16');
+        }
 
-            this.contract.methods.setMaxPollOptionCount(count)
-                .send({ from: this.account })
-                .on('receipt', (_receipt) => { resolve(_receipt); })
-                .on('error', (_error) => { reject(_error); });
-        });
-    }
+        this.contract.methods.setMaxPollOptionCount(count)
+            .send({ from: this.account })
+            .on('receipt', (_receipt) => { resolve(_receipt); })
+            .on('error', (_error) => { reject(_error); });
+    });
 
     // creates user account in dApp, given name of account holder
-    createUser(name) {
-        return new Promise((resolve, reject) => {
+    createUser = (name) => new Promise((resolve, reject) => {
 
-            this.contract.methods.createUser(name)
-                .send({ from: this.account })
-                .on('receipt', (_receipt) => { resolve(_receipt); })
-                .on('error', (_error) => { reject(_error); });
+        this.contract.methods.createUser(name)
+            .send({ from: this.account })
+            .on('receipt', (_receipt) => { resolve(_receipt); })
+            .on('error', (_error) => { reject(_error); });
 
-        });
-    }
+    });
 
     // creates poll with given topic, from invoker's account
-    createPoll(title) {
-        return new Promise((resolve, reject) => {
+    createPoll = (title) => new Promise((resolve, reject) => {
 
-            this.contract.methods.createPoll(title)
-                .send({ from: this.account })
-                .on('receipt', (_receipt) => { resolve(_receipt); })
-                .on('error', (_error) => { reject(_error); });
+        this.contract.methods.createPoll(title)
+            .send({ from: this.account })
+            .on('receipt', (_receipt) => { resolve(_receipt); })
+            .on('error', (_error) => { reject(_error); });
 
-        });
-    }
+    });
 
     // given unique identifier for poll, adds poll option i.e. 
     // option on which participants are going to cast their vote
