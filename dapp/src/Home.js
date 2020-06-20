ae64778d38fc74d3e6e870a1d2131eb47f8c1ea2
Anjan Roy <anjanroy@yandex.com>
2020-06-20 20:33:37 +0530

    Checks whether to navigate user to Register page or not

--- dapp/src/Home.js
+++ dapp/src/Home.js
@@ -8,8 +8,28 @@ class Home extends React.Component {
         this.state = { status: 'Welcome to Polling !', needToRegister: false }
     }
 
+    // called after first rendering of scene,
+    // decides whether to navigate user to Register page or not
+    async componentDidMount() {
+        try {
+            const res = await this.props.bridge.amIRegistered();
+
+            this.setState({ needToRegister: res });
+        } catch (e) {
+            this.setState({ status: e });
+        }
+
+    }
+
     render() {
 
+        if (this.needToRegister) {
+            return (
+                <div>
+                    <h1>To be implemented ...</h1>
+                </div>
+            )
+        }
         return (
             <div>
                 <h1>{this.state.status}</h1>
